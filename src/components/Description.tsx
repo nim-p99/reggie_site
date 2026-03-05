b73d099cb4dd9872339109962dcd9dddc5bb5dfc
Nimesh Patel <nimeshpatel99@hotmail.com>
2026-03-05 23:44:35 +0000

    mobile formatting

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,14 +4,14 @@ export default function Description() {
   return(
     <>
       <section className="relative px-6 py-16 md:py-32 flex flex-col items-center text-center">
-        <div className="max-w-4xl space-y-8">
+        <div className="max-w-4xl space-y-6 md:space-y-8">
           
-          <h2 className="text-3xl md:text-4xl tracking-tight">
+          <h2 className="text-2xl md:text-4xl tracking-tight">
             Meet Reggie, the only vegetarian <br className="hidden md:block" /> 
             T-rex in all of Heggie!
           </h2>
 
-          <div className="space-y-4 text-base md:text-lg leading-relaxed font-medium text-white/90">
+          <div className="space-y-4 text-sm md:text-lg leading-relaxed font-medium text-white/90">
             <p>
               Reggie refuses to eat meat like his dinosaur peers and T-rex family. 
               Throughout the book, Reggie is put down for not being like the rest of the dinosaurs. 
